ac32e93ce52f77a3557cca8b0b59db23a250ff64
TatianaShuvaeva <talisman1982@gmail.com>
2025-01-08 14:58:04 +0100

    Vergleichsoperatoren

--- js/script.js
+++ js/script.js
@@ -19,24 +19,76 @@
 
 // console.log(typeof (answer));
 
-const numberOfFilms = +prompt("Wie viele Filme haben Sie gesehen?", "");
+// const numberOfFilms = +prompt("Wie viele Filme haben Sie gesehen?", "");
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
 
-};
+// };
 
-const a = prompt("Einer der letzten Filme, die Sie gesehen haben?", "");
-const b = prompt("Wie würden Sie den Film bewerten?", "");
-const c = prompt("Einer der letzten Filme, die Sie gesehen haben?", "");
-const d = prompt("Wie würden Sie den Film bewerten?", "");
+// const a = prompt("Einer der letzten Filme, die Sie gesehen haben?", "");
+// const b = prompt("Wie würden Sie den Film bewerten?", "");
+// const c = prompt("Einer der letzten Filme, die Sie gesehen haben?", "");
+// const d = prompt("Wie würden Sie den Film bewerten?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(numberOfFilms);
-console.log(personalMovieDB);
+// console.log(numberOfFilms);
+// console.log(personalMovieDB);
+
+// console.log(NaN || 2 || undefined);
+
+
+// console.log(NaN && 2 && undefined);
+
+
+// console.log(1 && 2 && 3);
+
+
+// console.log(!1 && 2 || !3);
+
+
+// console.log(25 || null && !3);
+
+
+// console.log(NaN || null || !3 || undefined || 5);
+
+
+// console.log(NaN || null && !3 && undefined || 5);
+
+
+// console.log(5 === 5 && 3 > 1 || 5);
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+
+// if (hamburger === 3 && cola || fries === 3 && nuggets) {
+//     console.log('Done!')
+// };
+//     let hamburger;
+//     const fries = NaN;
+//     const cola = 0;
+//     const nuggets = 2;
+
+
+//     if (hamburger || cola || fries === 3 || nuggets) {
+//         console.log('Done!')
+//     }
+
+//     let hamburger;
+//     const fries = NaN;
+//     const cola = 0;
+//     const nuggets = 2;
+
+
+//     if (hamburger && cola || fries === 3 && nuggets) {
+//         console.log('Done!')
+//     }
