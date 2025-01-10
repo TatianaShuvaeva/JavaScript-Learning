3745d95e149992937365faf99197b709a0736197
TatianaShuvaeva <talisman1982@gmail.com>
2025-01-10 13:51:38 +0100

    Übungen mit Schleifen

--- js/script.js
+++ js/script.js
@@ -92,3 +92,140 @@
 //     if (hamburger && cola || fries === 3 && nuggets) {
 //         console.log('Done!')
 //     }
+
+// let result = '';
+// const length = 7;
+// for (let i = 1; i<length; i++) {
+//     for (let j=0; j<i; j++) {
+//         result += "*";
+//     }
+//     result += "\n";
+// }
+
+// console.log(result);
+
+// first: for (let i=0; i<3; i++) {
+//     console.log(`First level: ${i}`);
+//     for (let j=0; j<3; j++) {
+//         console.log(`Second level: ${j}`);
+//         for (let k=0; k<5; k++) {
+//             if (k===2) continue first;
+//             console.log(`Third level: ${k}`);
+//         }
+//     }
+// }
+
+// for (let zahl = 5; zahl < 11; zahl++) {
+//     console.log(zahl);
+// }    
+    
+
+//     for (let zahl = 20; zahl > 9; zahl--) {
+//         if (zahl === 13) break;
+//         console.log(zahl);
+//     }
+
+
+
+
+//     for (let zahl = 2; zahl < 11; zahl++) {
+//         if (zahl % 2 == 1) continue;
+//         console.log(zahl);
+//     }
+
+
+
+//     let i = 2;
+//     while (i <= 16) {
+//         i++;
+//         if (i % 2 === 0) {
+//             continue;
+//         } else {
+
+//             console.log(i);
+
+//         }
+//     }
+
+
+// const arrayOfNumbers = [];
+// let j = 5
+// for (let i = 0; i < 10; i++) {
+
+//     if (j > 10) break;
+//     arrayOfNumbers[i] = j;
+//     j++;
+
+
+// }
+// console.log(arrayOfNumbers);
+        
+    
+    
+// let i = 2;
+// while (i <= 15) {
+//     i++;
+//     if (i % 2 === 0) {
+//         continue;
+//     } else {
+
+//         console.log(i);
+
+//     }
+// } 
+
+
+// for (let i = 2; i <= 16; i++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+// const arr = [3, 5, 8, 16, 20, 23, 50];
+// const result = [];
+
+// const zahlElementen = arr.length
+
+// for (let i=0; i<zahlElementen; i++) {
+//     result[i] = arr[i];
+    
+// }
+// console.log(result);
+
+
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+// const zahlElementen = data.length
+
+
+
+// for (let i=0; i<zahlElementen; i++) {
+    
+//     if (typeof (data[i]) === "number") {
+//         data[i] = data[i] *2;
+//        } 
+   
+//     else {
+//         data[i] = data[i]+' - done'
+//     }
+     
+// }
+// console.log(data)
+
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+const result = [];
+
+const zahlElementen = data.length
+
+let j = 0;
+
+for (let i = zahlElementen-1; i >=0; i--) {
+    
+    result[j] = data[i];
+    j++;
+        
+
+}
+console.log(result)
