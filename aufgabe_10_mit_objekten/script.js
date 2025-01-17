a06cb4fe39bad9b2a7e74d31f62340ac2740c8d4
TatianaShuvaeva <talisman1982@gmail.com>
2025-01-17 14:44:10 +0100

    Namenveränderung

--- aufgabe_10_mit_objekten/script.js
+++ aufgabe_10_mit_objekten/script.js
@@ -54,3 +54,49 @@ function showProgrammingLangs(personalPlanPeter) {
     return result;
 }
 console.log(showProgrammingLangs(personalPlanPeter));
+
+
+// const personalPlanPeter = {
+//     name: "Peter",
+//     age: "29",
+//     skills: {
+//         languages: ['ru', 'eng'],
+//         programmingLangs: {
+//             js: '20%',
+//             php: '10%'
+//         },
+//         exp: '1 month'
+//     },
+//     showAgeAndLangs: function (plan) {
+//         const { age } = plan;
+//         const { languages } = plan.skills;
+//         let str = `Ich bin ${age} und spreche folgende Sprachen: `;
+
+//         languages.forEach(function (lang) {
+//             str += `${lang.toUpperCase()} `;
+//         });
+
+//         return str;
+//     }
+// };
+
+// personalPlanPeter.showAgeAndLangs(personalPlanPeter);
+
+// function showExperience(plan) {
+//     const { exp } = plan.skills;
+//     return exp;
+// }
+
+// showExperience(personalPlanPeter);
+
+// function showProgrammingLangs(plan) {
+//     let str = '';
+//     const { programmingLangs } = plan.skills;
+//     for (let key in programmingLangs) {
+//         str += `${key} wird zu ${programmingLangs[key]} erlernt\n`
+//     }
+
+//     return str;
+// }
+
+// showProgrammingLangs(personalPlanPeter);
