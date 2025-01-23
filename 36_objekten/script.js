2fa02cc33ee0127967a0077cf9c0ac26c2fdc425
TatianaShuvaeva <talisman1982@gmail.com>
2025-01-23 21:30:16 +0100

    class personalMovie hinzugefügt

--- 36_objekten/script.js
+++ 36_objekten/script.js
@@ -1,5 +1,105 @@
 'use strict';
 
+class personalMovie {
+    count = 0;
+    movies = {};
+    actors = {};
+    genres = [];
+    privat = false;
+
+    start() {
+        this.count = +prompt('Wie viele Filme haben Sie gesehen?', '');
+        console.log('Count: ', this.count);
+        while (this.count === '' || this.count == null || isNaN(this.count)) {
+
+            this.count = +prompt('Wie viele Filme haben Sie gesehen?', '');
+            console.log('Count: ', this.count);
+        }
+    }
+
+    rememberMyFilms() {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('Einer der letzten Filme, die Sie gesehen haben?', '').trim(),
+                b = prompt('Wie würden Sie den Film bewerten?', '');
+
+            if (a != '' && b != '' && a != null && b != null && a.length < 50) {
+
+                this.movies[a] = b;
+                console.log('done');
+            }
+            else {
+                console.log('error');
+                i--;
+            }
+
+        }
+    }
+
+    detectPersonalLevel() {
+        if (this.count < 10) {
+            console.log('Sehr wenige Filme gesehen');
+        } else if (this.count >= 10 && this.count < 30) {
+            console.log('Sie sind ein klassischer Zuschauer');
+        } else if (this.count >= 30) {
+            console.log('Sie sind ein Filmfan');
+        }
+        else {
+            console.log('Es ist ein Fehler aufgetreten');
+        }
+    }
+
+    showMyDB(hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    }
+
+    toggleVisibleMyDB() {
+        if (this.privat) {
+            this.privat = false;
+
+        } else {
+            this.privat = true;
+        }
+    }
+
+    writeYourGenres() {
+
+        for (let i = 1; i <= 3; i++) {
+            let genre = prompt(`Ihr Lieblingsgenre nummer ${i}?`);
+            console.log('Genre: ', genre)
+            if (genre) {
+                genre = genre.trim()
+            }
+
+            if (genre === '' || genre == null) {
+                console.log(`Sie haben falsche oder gar keine Daten eingegeben`);
+                i--;
+            }
+            else {
+                this.genres[i - 1] = genre;
+            }
+            // for (let i = 1; i <= 2; i++) {
+            // let genres = prompt(`Geben Sie bitte Ihre Lieblingsgenres durch Kommas getrennt ein`).toLowerCase();
+
+            // if (genres === '' || genres == null) {
+            //     console.log(`Sie haben falsche oder gar keine Daten eingegeben`);
+            //     i--;
+            // }
+            // else {
+            //     this.genres = genres.split(', ');
+            //     this.genres.sort();
+            // }
+
+        }
+        this.genres.forEach((item, i) => {
+            console.log(`Lieblingsgenre ${i + 1} ist ${item}`);
+        })
+    }
+
+}
+
+
 const personalMovieDB = {
     count: 0,
     movies: {},
@@ -7,10 +107,10 @@ const personalMovieDB = {
     genres: [],
     privat: false,
     start: function () {
-        personalMovieDB.count = +prompt('Wie viele Filme haben Sie gesehen?', '');
+        this.count = +prompt('Wie viele Filme haben Sie gesehen?', '');
 
         while (personalMovieDB.count === '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
-            personalMovieDB.count = +prompt('Wie viele Filme haben Sie gesehen?', '');
+            this.count = +prompt('Wie viele Filme haben Sie gesehen?', '');
         }
     },
     rememberMyFilms: function () {
@@ -57,10 +157,10 @@ const personalMovieDB = {
 
     },
     writeYourGenres: function () {
-      
+
         for (let i = 1; i <= 3; i++) {
             let genre = prompt(`Ihr Lieblingsgenre nummer ${i}?`);
-      
+
             if (genre === '' || genre == null) {
                 console.log(`Sie haben falsche oder gar keine Daten eingegeben`);
                 i--;
@@ -68,9 +168,9 @@ const personalMovieDB = {
             else {
                 personalMovieDB.genres[i - 1] = genre;
             }
-        // for (let i = 1; i <= 2; i++) {
+            // for (let i = 1; i <= 2; i++) {
             // let genres = prompt(`Geben Sie bitte Ihre Lieblingsgenres durch Kommas getrennt ein`).toLowerCase();
-      
+
             // if (genres === '' || genres == null) {
             //     console.log(`Sie haben falsche oder gar keine Daten eingegeben`);
             //     i--;
@@ -79,10 +179,10 @@ const personalMovieDB = {
             //     personalMovieDB.genres = genres.split(', ');
             //     personalMovieDB.genres.sort();
             // }
-         
+
         }
-        personalMovieDB.genres.forEach((item,i) => {
-            console.log(`Lieblingsgenre ${i+1} ist ${item}`);
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Lieblingsgenre ${i + 1} ist ${item}`);
         })
     }
 };
